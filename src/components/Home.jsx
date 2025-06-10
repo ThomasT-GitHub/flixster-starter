0138b8f60addd16d28caa0da0c31248b2714570a
Thomas Trivino <thomastrivino@meta.com>
2025-06-10 16:14:18 -0700

    Adds getNowpPlayingMovies to fetch the movies currently playing

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,3 +7,26 @@ const Home = () => {
   }
 
   export default Home
+
+async function getNowPlayingMovies() {
+    const url = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1';
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${import.meta.env.TMDB_API_KEY}`
+        }
+    };
+    try {
+        const response = await fetch(url, options);
+        if (!response.ok) {
+            throw new Error("Failed to fetch data!");
+        }
+
+        const data = await response.json();
+
+        return data;
+    } catch(error) {
+        console.error(error)
+    }
+}
