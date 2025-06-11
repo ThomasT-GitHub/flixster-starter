8eac6eac1bc2e2632eaa3f4145d2333222d5bb05
Thomas Trivino <thomastrivino@meta.com>
2025-06-10 17:22:56 -0700

    Sets the data from getNowPlayingMovies to movieList upon loading home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import MovieCard from './MovieCard'
+
 const Home = () => {
+    const [movieList, setMovieList] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const data = await getNowPlayingMovies();
+            setMovieList(data);
+        })();
+    }, []);
+
     return (
       <>
-        <p>Home</p>
+        {movieList.map((movie) => {
+            return <MovieCard key={movie.id} posterPath={movie.poster_path} title={movie.title} voterAverage={movie.voter_average}/>
+        })}
       </>
     )
   }
 
   export default Home
 
-async function getNowPlayingMovies() {
+  const getNowPlayingMovies = async () => {
     const url = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1';
     const options = {
         method: 'GET',
         headers: {
             accept: 'application/json',
-            Authorization: `Bearer ${import.meta.env.TMDB_API_KEY}`
+            Authorization: `Bearer ${import.meta.env.TMDB_ACESS_TOKEN}`
         }
     };
+
     try {
         const response = await fetch(url, options);
         if (!response.ok) {
@@ -25,7 +40,9 @@ async function getNowPlayingMovies() {
 
         const data = await response.json();
 
-        return data;
+        console.log(data)
+
+        return data.results;
     } catch(error) {
         console.error(error)
     }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,11 @@
-const MovieCard = ({imagePath, title, votes}) => {
-    const moviePosterSource = `https://image.tmdb.org/t/p/original/${imagePath}`;
+const MovieCard = ({posterPath, title, voterAverage}) => {
+    const moviePosterSource = `https://image.tmdb.org/t/p/original/${posterPath}`;
 
     return (
       <>
         <img src={moviePosterSource} alt="Movie Poster" />
         <h2>{title}</h2>
-        <p>{votes}</p>
+        <p>{voterAverage}</p>
       </>
     )
   }
